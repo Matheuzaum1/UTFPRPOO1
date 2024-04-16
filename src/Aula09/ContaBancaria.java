5c3970e25976bb2549fe9eacdf5906909d209bc6
Matheuzaum1 <matheushenriquerosendo@gmail.com>
2024-04-16 09:36:38 -0300

    Sua mensagem de commit

--- src/Aula09/ContaBancaria.java
+++ src/Aula09/ContaBancaria.java
@@ -1,41 +1,65 @@
 package Aula09;
 
 public class ContaBancaria {
-	
-	int numeroConta;
-	String titular;
-	double saldo;
-	
-	
-	//Metodo Construtor
+
+	private int numeroConta;
+	private String titular;
+	private double saldo;
+
 	public ContaBancaria(int numeroConta, String titular) {
+
 		this.numeroConta = numeroConta;
 		this.titular = titular;
 		this.saldo = 0.00;
 	}
-	
+
 	public void sacar(double valor) {
+
 		if (valor <= this.saldo) {
+
 			this.saldo -= valor;
-			System.out.println("Saque realizado com sucesso!");
-		}else {
-			System.out.println("Saldo Insuficiente!");
+			System.out.println("Saque realizado com sucesso.");
+		
+		} else {
+		
+			System.out.println("Saldo insuficiente.");
 		}
-			
 	}
+
 	public void depositar(double valor) {
+
 		this.saldo += valor;
-		System.out.println("Valor Depositado com sucesso!");
+		System.out.println("Dep�sito realizado com sucesso.");
+
+		this.calcularRendimento();
 	}
+
 	public void imprimirSaldo() {
-		System.out.println("====================");
-		System.out.println("Número da conta: " + this.numeroConta);
-		System.out.println("Nome do titular: " + this.titular);
-		System.out.println("Saldo : R$ " + this.saldo);
-		System.out.println("====================");
+
+		System.out.println("======================");
+		System.out.println("N�mero da Conta: " + this.numeroConta);
+		System.out.println("Nome do Titular: " + this.titular);
+		System.out.println("Saldo: R$" + this.saldo);
+		System.out.println("======================");
 	}
-	public void calcularRendimento() {
+
+	private void calcularRendimento() {
+
 		this.saldo *= 1.10;
-				
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+
+		if (!titular.equals("")) {
+
+			this.titular = titular;
+		}
 	}
 }
--- src/Aula09/ContaBancariaTeste.java
+++ src/Aula09/ContaBancariaTeste.java
@@ -1,12 +1,22 @@
 package Aula09;
 
 public class ContaBancariaTeste {
-	public static void main() {
-		
-		ContaBancaria cb1 = new ContaBancaria(111, "João da Silva");
+
+	public static void main(String[] args) {
+
+		ContaBancaria cb1 = new ContaBancaria(111, "Jo�o da Silva");
 		cb1.imprimirSaldo();
-		
+
 		cb1.depositar(500.00);
+		cb1.imprimirSaldo();
+
+		cb1.sacar(300.00);
+		cb1.imprimirSaldo();
+
+		// alterando o nome do titular por meio dos m�todos de acesso
+		cb1.setTitular("Jo�o da Silva Nascimento"); // alterando o nome do titular da conta
+		System.out.println("Nome Alterado: " + cb1.getTitular()); // obtendo o nome do titular da conta
+
 		cb1.imprimirSaldo();
 	}
 }
