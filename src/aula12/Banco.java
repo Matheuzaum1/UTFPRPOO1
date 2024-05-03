eec757a333288238ca70bea74cf3c52d774ba2b8
Matheus Henrique Rosendo Medeiros <a2605970@campus.pg.utfpr.edu.br>
2024-05-03 20:05:56 -0300

    Aula12

---
+++ src/aula12/Banco.java
@@ -0,0 +1,65 @@
+package aula12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+    private String cnpj;
+    private String nomeBanco;
+    private List <ContaBancaria> contas;
+    
+    
+    public Banco(String cnpj, String nomeBanco) {
+        this.cnpj = cnpj;
+        this.nomeBanco = nomeBanco;
+        this.contas = new ArrayList<ContaBancaria>();
+    }
+    public void criarNovaConta (int numeroConta, String titular){
+        ContaBancaria novaConta = new ContaBancaria(numeroConta, titular);
+        this.contas.add(novaConta);
+
+        System.out.println("Conta " + numeroConta + " criada com sucesso!");
+    }
+
+    public void verificarSaldo(int numeroConta){
+
+        for (ContaBancaria contaBancaria : contas){
+            if (contaBancaria.getNumeroConta() == numeroConta){
+                contaBancaria.imprimirSaldo();
+                return;
+            }
+        }
+        System.out.println("Conta não encontrada!");
+    }
+
+    public void depositarConta (int numeroConta, double valor){
+        for (ContaBancaria contaBancaria : contas){
+            if (contaBancaria.getNumeroConta() == numeroConta){
+                contaBancaria.depositar(valor);
+                return;
+            }
+        }
+        System.out.println("Conta não encontrada!");
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void listarContas(){
+        for (ContaBancaria contaBancaria : contas) {
+            contaBancaria.imprimirSaldo();            
+        }
+    }
+}
---
+++ src/aula12/BancoTeste.java
@@ -0,0 +1,18 @@
+package aula12;
+
+public class BancoTeste {
+
+	public static void main(String[] args) {
+
+		Banco banco1 = new Banco ("123456", "NewBank");
+
+		banco1.criarNovaConta(111, "João da Silva");
+		banco1.criarNovaConta(222, "Maria da Silva");
+		banco1.criarNovaConta(333, "José da Silva");
+		banco1.criarNovaConta(444, "Pedro da Silva");
+
+		banco1.verificarSaldo(222);
+		banco1.depositarConta(222,500);
+		banco1.verificarSaldo(222);
+	}
+}
